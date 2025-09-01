5f0da47a0244b3828efba621eaf8e1986699ec58
James Bradshaw <james.g.bradshaw@gmail.com>
2025-09-01 15:33:34 +0000

    add docs on registered hooks

--- src/gunicorn_django_wide_events/gunicorn_hooks/registered_hooks.py
+++ src/gunicorn_django_wide_events/gunicorn_hooks/registered_hooks.py
@@ -1,3 +1,10 @@
+"""Provides gunicorn server hook definitions that run all registered callbacks
+
+Usage: Add `from gunicorn_django_wide_events.registered_hook import *` to your gunicorn config file.
+
+N.B.: You might _want_ to metaprogram these...but gunciorn does arity-checking, so no *args, **kwargs shenanigans!
+"""
+
 from gunicorn_django_wide_events.gunicorn_hooks.hooks import Hook, hook_registry
 
 
