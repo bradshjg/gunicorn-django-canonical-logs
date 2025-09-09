4f63d10b028402ff08591ab43c461ffca891f17f
James Bradshaw <james.g.bradshaw@gmail.com>
2025-09-09 04:53:52 +0000

    Fix ordering issues, need to ensure a clean Context

--- tests/instrumenter_tests/test_database.py
+++ tests/instrumenter_tests/test_database.py
@@ -17,6 +17,7 @@ def instrumenter() -> Generator[DatabaseInstrumenter, None, None]:
 
 
 def test_queries(instrumenter, live_server):
+    Context.reset()
     url = live_server + "/db_queries/"
     resp = requests.get(url)
     assert resp.status_code == 200
@@ -32,6 +33,7 @@ def test_queries(instrumenter, live_server):
 
 
 def test_reset(instrumenter, live_server):
+    Context.reset()
     url = live_server + "/db_queries/"
     resp = requests.get(url)
     assert resp.status_code == 200
