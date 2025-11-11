a2ac1f5d9dfa69448c65bc02011108eb901a5a99
James Bradshaw <james.g.bradshaw@gmail.com>
2025-11-11 06:54:15 +0000

    implement partial failure `on_error` decorator

---
+++ src/gunicorn_django_canonical_logs/partial_failure.py
@@ -0,0 +1,39 @@
+import sys
+import traceback
+from functools import wraps
+
+from django.conf import settings
+
+from gunicorn_django_canonical_logs.event_context import Context, EventContext
+from gunicorn_django_canonical_logs.stack_context import get_stack_loc_context
+from gunicorn_django_canonical_logs.logfmt import LogFmt
+
+def on_error(*, return_value):
+    def decorator(function):
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            try:
+                retval = function(*args, **kwargs)
+            except Exception:
+                if settings.DEBUG:
+                    raise
+                exc_type, exc_value, tb = sys.exc_info()
+                if exc_type and exc_value and tb:
+                    exc_context: dict[str, str | None] = {
+                        "type": exc_type.__name__,
+                        "msg": str(exc_value),
+                    }
+
+                    loc_context = get_stack_loc_context(traceback.extract_tb(tb))
+                    exc_context.update(loc_context)
+                    partial_failure_context = EventContext()
+                    request_id = Context.get("id", namespace="req")
+                    partial_failure_context.set("type", "partial_failure", namespace="event")
+                    partial_failure_context.set("id", request_id, namespace="req")
+                    partial_failure_context.update(context=exc_context, namespace="exc")
+                    print(LogFmt.format(partial_failure_context))
+                    return return_value
+            else:
+                return retval
+        return wrapper
+    return decorator
